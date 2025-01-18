d8efb805fdecdedc39868cb80a4fff84fbb3ec32
vaibhavthapli <hellovaibt@gmail.com>
2025-01-18 13:54:47 +0530

    update

--- template.py
+++ template.py
@@ -20,7 +20,7 @@ list_of_files = [
     f"src/{project_name}/exception.py",
     f"src/{project_name}/logger.py",
     f"src/{project_name}/utils/__init__.py",
-   # f"src/{project_name}/utils/utils.py",
+    f"src/{project_name}/utils/utils.py",
     #f"src/{project_name}/config/__init__.py",
     #f"src/{project_name}/config/configuration.py",
     #f"src/{project_name}/constants/__init__.py",
