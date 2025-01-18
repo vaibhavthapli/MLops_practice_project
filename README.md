4dd7f46127c8602256e35aefe773a361830a16e1
vaibhavthapli <hellovaibt@gmail.com>
2025-01-18 13:52:59 +0530

    update

--- README.md
+++ README.md
@@ -6,8 +6,10 @@ Thank You!
 STEPS:
 1. create requirements.txt
 2. create setup.py
-3. before run setup.py create src package(by adding __init__.py) in a src folder
+3. before run setup.py '''create src package(by adding __init__.py) in a src folder for check'''
 4. run setup.py
 ###### components: components are the steps we follows in training pipeline(data source,data ingestion, data transformation,model trainer,model monitoring,CI/CD pipeline,model deployment)
 5. create template.py  (responsible for creating the entire project structure)
-6. 
+6. code in template.py  # we have to things in our project [components & pipeline(training, testing)]
+7. run template.py  (python template.py)
+8. 
---
+++ template.py
@@ -0,0 +1,47 @@
+import os
+from pathlib import Path  #create path
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+project_name = "mlproject"
+
+list_of_files = [
+    #".github/workflows/.gitkeep",  #for deployment in git action
+    f"src/{project_name}/__init__.py",
+    f"src/{project_name}/components/__init__.py",
+    f"src/{project_name}/components/data_ingestion.py",
+    f"src/{project_name}/components/data_transformation.py",
+    f"src/{project_name}/components/model_trainer.py",
+    f"src/{project_name}/components/model_monitering.py",
+    f"src/{project_name}/pipelines/__init__.py",
+    f"src/{project_name}/pipelines/training_pipeline.py",
+    f"src/{project_name}/pipelines/prediction_pipeline.py",
+    f"src/{project_name}/exception.py",
+    f"src/{project_name}/logger.py",
+    f"src/{project_name}/utils/__init__.py",
+   # f"src/{project_name}/utils/utils.py",
+    #f"src/{project_name}/config/__init__.py",
+    #f"src/{project_name}/config/configuration.py",
+    #f"src/{project_name}/constants/__init__.py",
+   # "config/config.yaml",
+   # "params.yaml",
+    "app.py",
+    "Dockerfile",
+]
+
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    filedir, filename = os.path.split(filepath)
+
+    if filedir != "":
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"Creating directory; {filedir} for the file: {filename}")
+
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath, "w") as f:
+            pass
+            logging.info(f"Creating empty file: {filepath}")
+
+    else:
+        logging.info(f"Creating empty file: {filepath}")
